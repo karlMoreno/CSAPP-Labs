7d5903f3f2e225db46d33acfb71951a87c4f9ba7
karlcmoreno0811 <karlcmoreno0811@gmail.com>
2025-12-22 19:34:34 +0000

    conditional

--- Data-Lab/datalab-handout/bits.c
+++ Data-Lab/datalab-handout/bits.c
@@ -235,7 +235,13 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  int notZero = !!x;
+  int selectMask = ~notZero + 1;
+  
+  int selectY = selectMask & y;
+  int selectZ = ~selectMask & z;
+
+  return selectY | selectZ;
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
